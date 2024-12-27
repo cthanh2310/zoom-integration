9333b7f7997fb9ef468d0974071be258a09fd320
Kris <thanh.sosc@carehealth.io>
2024-12-27 18:26:39 +0700

    test

---
+++ videosdk-ui-toolkit-javascript-sample copy/scripts.js
@@ -0,0 +1,52 @@
+import uitoolkit from './@zoom/videosdk-ui-toolkit/index.js'
+
+var sessionContainer = document.getElementById('sessionContainer')
+var authEndpoint = 'http://localhost:3333/create-session'
+var config = {
+    videoSDKJWT: '',
+    sessionName: 'Kris and his friends 10',
+    userName: 'JavaScript',
+    sessionPasscode: '123456',
+    features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share'],
+    options: { init: {}, audio: {}, video: {}, share: {}},
+    virtualBackground: {
+       allowVirtualBackground: true,
+       allowVirtualBackgroundUpload: true,
+       virtualBackgrounds: ['https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop']
+    }
+};
+var role = 1
+
+window.getVideoSDKJWT = getVideoSDKJWT
+
+function getVideoSDKJWT() {
+    console.log('Tracking...')
+    document.getElementById('join-flow').style.display = 'none'
+
+    fetch(authEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sessionName: config.sessionName, sessionPasscode: config.sessionPasscode})
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        config.videoSDKJWT = data.fe_token
+        joinSession()
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+function joinSession() {
+    uitoolkit.joinSession(sessionContainer, config)
+
+    uitoolkit.onSessionClosed(sessionClosed)
+}
+
+var sessionClosed = (() => {
+    console.log('session closed')
+    uitoolkit.closeSession(sessionContainer)
+
+    document.getElementById('join-flow').style.display = 'block'
+})
